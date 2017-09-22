f8bb93d72dff533bf8af28e6b67df2a8da835eb9
Chase (Ryan Cline) <ryancline88@yahoo.com>
2017-09-22 17:27:43 -0600

    Update

--- breakout_game/script.js
+++ breakout_game/script.js
@@ -8,7 +8,41 @@ var ballRadius = 10
 var paddleHeight = 10
 var paddleWidth = 75
 var paddleX = (canvas.width-paddleWidth)/2
+var rightPressed = false
+var leftPressed = false
+var brickRowCount = 3
+var brickColumnCount = 5
+var brickWidth = 75
+var brickHeight = 20
+var brickPadding = 10
+var brickOffsetTop = 30
+var brickOffsetLeft = 30
+var bricks = []
+for (c=0; c<brickColumnCount; c++) {
+  bricks[c] = []
+  for (r=0; r<brickRowCount; r++) {
+    bricks[c][r] = {x: 0, y: 0}
+  }
+}
 
+document.addEventListener('keydown', keyDownHandler, false)
+document.addEventListener('keyup', keyUpHandler, false)
+
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = true
+  } else if (e.keyCode == 37) {
+    leftPressed = true
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = false
+  } else if (e.keyCode == 37) {
+    leftPressed = false
+  }
+}
 
 function drawBall() {
   ctx.beginPath()
@@ -26,13 +60,37 @@ function drawPaddle() {
   ctx.closePath()
 }
 
+function drawBricks() {
+  for (c=0; c<brickColumnCount; c++) {
+    for (r=0; r<brickRowCount; r++) {
+      var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft
+      var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop
+      bricks[c][r].x = brickX
+      bricks[c][r].y = brickY
+      ctx.beginPath()
+      ctx.rect(brickX, brickY, brickWidth, brickHeight)
+      ctx.fillStyle = '#0095DD'
+      ctx.fill()
+      ctx.closePath()
+    }
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawBricks()
   drawBall()
   drawPaddle()
 
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+  if (y + dy < ballRadius) {
     dy = -dy
+  } else if (y + dy > canvas.height - ballRadius - paddleHeight) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy
+    } else if (y + dy > canvas.height - ballRadius) {
+      alert("GAME OVER")
+      document.location.reload()
+    }
   }
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx
@@ -40,7 +98,12 @@ function draw() {
 
   x += dx
   y += dy
-}
 
+  if (rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7
+  }
+}
 
 setInterval(draw, 10)
